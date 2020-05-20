3f752061cf7f3e63e2234de835102a190b7eab51
lpanonymous <lpanonymous0101@gmail.com>
2020-05-20 16:15:55 -0500

    peso

--- index.php
+++ index.php
@@ -1,27 +1,68 @@
 <?php
     header("Content-Type:application/json");
     $nombre = $_GET['nombre'];
-    if(!empty($nombre))
+    $nombrePlaneta = $_GET['nombrePlaneta'];
+    if(!empty($nombre) && !empty($nombrePlaneta))
     {
         //Nombre valido
         $peso = $_GET['peso'];
-        $estatura = $_GET['estatura'];
-        if(!empty($peso) && !empty($estatura))
+        if(!empty($peso))
         {
-            $estatura /= 100;
-            $imc = $peso / ($estatura * $estatura);
-            $imc = round($imc, 2);
-            deliver_response(200, "$nombre tu IMC es de $imc", $imc);
+          switch ($nombrePlaneta)
+          {
+            case "MERCURIO":
+              $pesoplaneta = $peso * 3.70;
+              $pesoplaneta = round($pesoplaneta, 2);
+              deliver_response(200, "$nombre tu peso en el planeta Mercurio es de $pesoplaneta", $pesoplaneta);
+            break;
+            case "VENUS":
+              $pesoplaneta = $peso * 8.87;
+              $pesoplaneta = round($pesoplaneta, 2);
+              deliver_response(200, "$nombre tu peso en el planeta Venus es de $pesoplaneta", $pesoplaneta);
+            break;
+            case "MARTE":
+              $pesoplaneta = $peso * 3.71;
+              $pesoplaneta = round($pesoplaneta, 2);
+              deliver_response(200, "$nombre tu peso en el planeta Marte es de $pesoplaneta", $pesoplaneta);
+            break;
+            case "JUPITER":
+              $pesoplaneta = $peso * 23.12;
+              $pesoplaneta = round($pesoplaneta, 2);
+              deliver_response(200, "$nombre tu peso en el planeta Jupiter es de $pesoplaneta", $pesoplaneta);
+            break;
+            case "SATURNO":
+              $pesoplaneta = $peso * 8.96;
+              $pesoplaneta = round($pesoplaneta, 2);
+              deliver_response(200, "$nombre tu peso en el planeta Saturno es de $pesoplaneta", $pesoplaneta);
+            break;
+            case "URANO":
+              $pesoplaneta = $peso * 8.69;
+              $pesoplaneta = round($pesoplaneta, 2);
+              deliver_response(200, "$nombre tu peso en el planeta Urano es de $pesoplaneta", $pesoplaneta);
+            break;
+            case "NEPTUNO":
+              $pesoplaneta = $peso * 11;
+              $pesoplaneta = round($pesoplaneta, 2);
+              deliver_response(200, "$nombre tu peso en el planeta Neptuno es de $pesoplaneta", $pesoplaneta);
+            break;
+            case "PLUTON":
+              $pesoplaneta = $peso * 0.81;
+              $pesoplaneta = round($pesoplaneta, 2);
+              deliver_response(200, "$nombre tu peso en el planeta Pluton es de $pesoplaneta", $pesoplaneta);
+            break;
+            default:
+            echo "Your favorite color is neither red, blue, nor green!";
+          }
         }
         else
         {
-            deliver_response(200, "peso o estatura no validos", null);
+            deliver_response(200, "peso no valido", null);
         }
     }
     else
     {
         //Nombre no valido
-        deliver_response(200, "Nombre no valido", null);
+        deliver_response(200, "Nombre no valido y nombre de planeta no valido", null);
     }
 
     function deliver_response($status, $status_message, $data)
